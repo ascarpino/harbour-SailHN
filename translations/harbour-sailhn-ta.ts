d665343e9da0151711a15dd67c8f5336fad179a1
தமிழ்நேரம் <anishprabu.t@gmail.com>
2025-01-02 12:12:45 +0000

    Translated using Weblate (Tamil)
    Currently translated at 100.0% (45 of 45 strings)
    
    Translation: Harbour SailHN/Translations
    Translate-URL: https://hosted.weblate.org/projects/harbour-sailhn/translations/ta/

--- translations/harbour-sailhn-ta.ts
+++ translations/harbour-sailhn-ta.ts
@@ -6,37 +6,37 @@
     <message>
         <location filename="../qml/pages/CommentsPage.qml" line="54"/>
         <source>Copy external URL</source>
-        <translation type="unfinished"></translation>
+        <translation>வெளிப்புற முகவரி ஐ நகலெடுக்கவும்</translation>
     </message>
     <message>
         <location filename="../qml/pages/CommentsPage.qml" line="62"/>
         <source>Share</source>
-        <translation type="unfinished"></translation>
+        <translation>பங்கு</translation>
     </message>
     <message>
         <location filename="../qml/pages/CommentsPage.qml" line="71"/>
         <source>Reply</source>
-        <translation type="unfinished"></translation>
+        <translation>பதில்</translation>
     </message>
     <message>
         <location filename="../qml/pages/CommentsPage.qml" line="78"/>
         <source>Refresh</source>
-        <translation type="unfinished"></translation>
+        <translation>புதுப்பிப்பு</translation>
     </message>
     <message>
         <location filename="../qml/pages/CommentsPage.qml" line="91"/>
         <source>Load more</source>
-        <translation type="unfinished"></translation>
+        <translation>மேலும் ஏற்றவும்</translation>
     </message>
     <message>
         <location filename="../qml/pages/CommentsPage.qml" line="107"/>
         <source>Comments</source>
-        <translation type="unfinished"></translation>
+        <translation>கருத்துகள்</translation>
     </message>
     <message>
         <location filename="../qml/pages/CommentsPage.qml" line="200"/>
         <source>Share URL</source>
-        <translation type="unfinished"></translation>
+        <translation>முகவரி ஐப் பகிரவும்</translation>
     </message>
 </context>
 <context>
@@ -44,22 +44,22 @@
     <message>
         <location filename="../qml/pages/ItemDelegate.qml" line="56"/>
         <source>point</source>
-        <translation type="unfinished"></translation>
+        <translation>புள்ளியம்</translation>
     </message>
     <message>
         <location filename="../qml/pages/ItemDelegate.qml" line="58"/>
         <source>points</source>
-        <translation type="unfinished"></translation>
+        <translation>பிரிவகம்</translation>
     </message>
     <message>
         <location filename="../qml/pages/ItemDelegate.qml" line="64"/>
         <source>comment</source>
-        <translation type="unfinished"></translation>
+        <translation>கருத்து</translation>
     </message>
     <message>
         <location filename="../qml/pages/ItemDelegate.qml" line="66"/>
         <source>comments</source>
-        <translation type="unfinished"></translation>
+        <translation>கருத்துகள்</translation>
     </message>
 </context>
 <context>
@@ -67,22 +67,22 @@
     <message>
         <location filename="../qml/pages/RepliesPage.qml" line="47"/>
         <source>Reply</source>
-        <translation type="unfinished"></translation>
+        <translation>பதில்</translation>
     </message>
     <message>
         <location filename="../qml/pages/RepliesPage.qml" line="54"/>
         <source>Refresh</source>
-        <translation type="unfinished"></translation>
+        <translation>புதுப்பிப்பு</translation>
     </message>
     <message>
         <location filename="../qml/pages/RepliesPage.qml" line="67"/>
         <source>Load more</source>
-        <translation type="unfinished"></translation>
+        <translation>மேலும் ஏற்றவும்</translation>
     </message>
     <message>
         <location filename="../qml/pages/RepliesPage.qml" line="83"/>
         <source>Replies</source>
-        <translation type="unfinished"></translation>
+        <translation>பதில்கள்</translation>
     </message>
 </context>
 <context>
@@ -90,27 +90,27 @@
     <message>
         <location filename="../qml/pages/Reply.qml" line="44"/>
         <source>Error when sending</source>
-        <translation type="unfinished"></translation>
+        <translation>அனுப்பும்போது பிழை</translation>
     </message>
     <message>
         <location filename="../qml/pages/Reply.qml" line="46"/>
         <source>Commented!</source>
-        <translation type="unfinished"></translation>
+        <translation>கருத்து!</translation>
     </message>
     <message>
         <location filename="../qml/pages/Reply.qml" line="61"/>
         <source>Reply</source>
-        <translation type="unfinished"></translation>
+        <translation>பதில்</translation>
     </message>
     <message>
         <location filename="../qml/pages/Reply.qml" line="68"/>
         <source>Text</source>
-        <translation type="unfinished"></translation>
+        <translation>உரை</translation>
     </message>
     <message>
         <location filename="../qml/pages/Reply.qml" line="73"/>
         <source>Add Comment</source>
-        <translation type="unfinished"></translation>
+        <translation>கருத்து சேர்க்கவும்</translation>
     </message>
 </context>
 <context>
@@ -118,17 +118,17 @@
     <message>
         <location filename="../qml/pages/ReplyDelegate.qml" line="61"/>
         <source>reply</source>
-        <translation type="unfinished"></translation>
+        <translation>பதில்</translation>
     </message>
     <message>
         <location filename="../qml/pages/ReplyDelegate.qml" line="63"/>
         <source>replies</source>
-        <translation type="unfinished"></translation>
+        <translation>பதில்கள்</translation>
     </message>
     <message>
         <location filename="../qml/pages/ReplyDelegate.qml" line="77"/>
         <source>Reply</source>
-        <translation type="unfinished"></translation>
+        <translation>பதில்</translation>
     </message>
 </context>
 <context>
@@ -136,58 +136,58 @@
     <message>
         <location filename="../qml/pages/Settings.qml" line="61"/>
         <source>Log out</source>
-        <translation type="unfinished"></translation>
+        <translation>விடுபதிகை</translation>
     </message>
     <message>
         <location filename="../qml/pages/Settings.qml" line="113"/>
         <source>Login failed</source>
-        <translation type="unfinished"></translation>
+        <translation>உள்நுழைவு தோல்வியடைந்தது</translation>
     </message>
     <message>
         <location filename="../qml/pages/Settings.qml" line="76"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>அமைப்புகள்</translation>
     </message>
     <message>
         <location filename="../qml/pages/Settings.qml" line="82"/>
         <source>Username</source>
-        <translation type="unfinished"></translation>
+        <translation>பயனர்பெயர்</translation>
     </message>
     <message>
         <location filename="../qml/pages/Settings.qml" line="90"/>
         <source>Password</source>
-        <translation type="unfinished"></translation>
+        <translation>கடவுச்சொல்</translation>
     </message>
     <message>
         <location filename="../qml/pages/Settings.qml" line="98"/>
         <location filename="../qml/pages/Settings.qml" line="186"/>
         <source>Login</source>
-        <translation type="unfinished"></translation>
+        <translation>புகுபதிவு</translation>
     </message>
     <message>
         <location filename="../qml/pages/Settings.qml" line="130"/>
         <source>User details</source>
-        <translation type="unfinished"></translation>
+        <translation>பயனர் விவரங்கள்</translation>
     </message>
     <message>
         <location filename="../qml/pages/Settings.qml" line="136"/>
         <source>created</source>
-        <translation type="unfinished"></translation>
+        <translation>உருவாக்கப்பட்டது</translation>
     </message>
     <message>
         <location filename="../qml/pages/Settings.qml" line="142"/>
         <source>karma</source>
-        <translation type="unfinished"></translation>
+        <translation>கர்மா</translation>
     </message>
     <message>
         <location filename="../qml/pages/Settings.qml" line="148"/>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>பற்றி</translation>
     </message>
     <message>
         <location filename="../qml/pages/Settings.qml" line="184"/>
         <source>Logged</source>
-        <translation type="unfinished"></translation>
+        <translation>உள்நுழைந்த</translation>
     </message>
 </context>
 <context>
@@ -195,22 +195,22 @@
     <message>
         <location filename="../qml/pages/StoriesListView.qml" line="49"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>அமைப்புகள்</translation>
     </message>
     <message>
         <location filename="../qml/pages/StoriesListView.qml" line="56"/>
         <source>Submit</source>
-        <translation type="unfinished"></translation>
+        <translation>சமர்ப்பிக்கவும்</translation>
     </message>
     <message>
         <location filename="../qml/pages/StoriesListView.qml" line="63"/>
         <source>Refresh</source>
-        <translation type="unfinished"></translation>
+        <translation>புதுப்பிப்பு</translation>
     </message>
     <message>
         <location filename="../qml/pages/StoriesListView.qml" line="72"/>
         <source>Load more</source>
-        <translation type="unfinished"></translation>
+        <translation>மேலும் ஏற்றவும்</translation>
     </message>
 </context>
 <context>
@@ -218,38 +218,38 @@
     <message>
         <location filename="../qml/pages/Submit.qml" line="45"/>
         <source>Submitted!</source>
-        <translation type="unfinished"></translation>
+        <translation>சமர்ப்பிக்கப்பட்டது!</translation>
     </message>
     <message>
         <location filename="../qml/pages/Submit.qml" line="60"/>
         <location filename="../qml/pages/Submit.qml" line="96"/>
         <source>Submit</source>
-        <translation type="unfinished"></translation>
+        <translation>சமர்ப்பிக்கவும்</translation>
     </message>
     <message>
         <location filename="../qml/pages/Submit.qml" line="67"/>
         <source>Title</source>
-        <translation type="unfinished"></translation>
+        <translation>தலைப்பு</translation>
     </message>
     <message>
         <location filename="../qml/pages/Submit.qml" line="75"/>
         <source>Url</source>
-        <translation type="unfinished"></translation>
+        <translation>முகவரி</translation>
     </message>
     <message>
         <location filename="../qml/pages/Submit.qml" line="81"/>
         <source>or</source>
-        <translation type="unfinished"></translation>
+        <translation>அல்லது</translation>
     </message>
     <message>
         <location filename="../qml/pages/Submit.qml" line="89"/>
         <source>Text</source>
-        <translation type="unfinished"></translation>
+        <translation>உரை</translation>
     </message>
     <message>
         <location filename="../qml/pages/Submit.qml" line="43"/>
         <source>Error during submission</source>
-        <translation type="unfinished"></translation>
+        <translation>சமர்ப்பிக்கும் போது பிழை</translation>
     </message>
 </context>
 </TS>
